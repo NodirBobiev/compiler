293c40a20ea9f4090f61b70728a3f9d4c4c5ffd7
NodirBobiev <nodir.bobiev@gmail.com>
2023-05-12 11:45:34 +0300

    implement Type class

---
+++ common/type.cpp
@@ -0,0 +1,59 @@
+#include <string>
+
+class Type {
+public:
+    virtual ~Type() {}
+};
+
+class TypeInteger : public Type {
+public:
+    TypeInteger(){}
+};
+
+class TypeBoolean : public Type {
+public:
+    TypeBoolean(){}
+};
+
+class TypeReal : public Type {
+public:
+    TypeReal(){}
+};
+
+class TypeRoutine : public Type {
+public:
+    TypeRoutine(){}
+};
+
+
+std::string toString(Type* type) 
+{
+    if( type == nullptr )
+        return "TYPENULL";
+    if( auto t = dynamic_cast<TypeBoolean*>(type))
+        return "BOOLEAN";
+    if( auto t = dynamic_cast<TypeInteger*>(type))
+        return "INTEGER";
+    if( auto t = dynamic_cast<TypeReal*>(type))
+        return "REAL";
+    throw std::runtime_error("toString: type is not implemented");
+}
+
+
+Type *mapType(Node *node) 
+{
+    if( node->type == "type" && node->children[0]->type == "primitive_type" ) {
+        auto value = node->children[0]->value;
+        if( value == "integer" )
+            return new TypeInteger();
+        if( value == "boolean" )
+            return new TypeBoolean();
+        if( value == "real" )
+            return new TypeReal();
+        else
+            throw std::runtime_error("mapType: Primitive type " + value + " is not supported");
+    }
+    else{
+        throw std::runtime_error("mapType: Node with " + node->children[0]->value + " type is not implemented");
+    }
+}
